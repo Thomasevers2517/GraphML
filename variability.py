c9b35552d0e3a3407ff63fe3cd1f685889e96dbe
AnjaKroon <68068874+AnjaKroon@users.noreply.github.com>
2024-05-30 17:30:14 -0400

    new file

---
+++ variability.py
@@ -0,0 +1,8 @@
+# Objective: Look at graph signal variability across the mobility data on a week and daily basis
+# Objective: Investigate approaches to reduce the variability of the graph signal 
+
+# ANALYZING WEEKLY DATA - COUNTY BY COUNTY
+
+# Pull in graph data from various weeks
+
+
